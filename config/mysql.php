7a211298c2499d9650415e668e842d2f1e8d9bb3
Polinkot <pk_work@mail.ru>
2020-06-13 20:10:26 +0400

    Module2. DZ1

--- config/mysql.php
+++ config/mysql.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => yii\db\Connection::class,
-    'dsn' => 'mysql:host=mysql:3306;dbname=mysql_db',
-    'username' => 'root',
-    'password' => 'password',
+    'dsn' => 'mysql:host=' . ($_ENV['MYSQL_HOST'] ?? 'mysql') . ':3306;dbname=' . ($_ENV['MYSQL_DB'] ?? 'mysql_db'),
+    'username' => $_ENV['MYSQL_USER'] ?? 'root',
+    'password' => $_ENV['MYSQL_PASSWORD'] ?? 'password',
     'charset' => 'utf8',
 ];
